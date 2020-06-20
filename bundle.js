3c6648f63ffc38e3bb221a33510750ed50c4ac2a
unknown <lucas_hideki211@hotmail.com>
2020-06-20 17:18:55 -0300

    Exercicio 3

--- bundle.js
+++ bundle.js
@@ -1,45 +1,41 @@
 "use strict";
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+// 3.1
+var arr = [1, 2, 3, 4, 5];
+arr.map(function (item) {
+  return item + 10;
+}); // 3.2
+// Dica: Utilize uma constante pra function
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-var usuarios = [{
+var usuario = {
   nome: 'Diego',
-  idade: 23,
-  empresa: 'Rocketseat'
-}, {
-  nome: 'Gabriel',
-  idade: 15,
-  empresa: 'Rocketseat'
-}, {
-  nome: 'Lucas',
-  idade: 30,
-  empresa: 'Facebook'
-}]; //2.1
+  idade: 23
+};
 
-var idadeArr = usuarios.map(function (item) {
-  return item.idade;
-});
-console.log(idadeArr); //2.2
+var mostraIdade = function mostraIdade(usuario) {
+  return usuario.idade;
+};
 
-var eMaior = usuarios.filter(function (item) {
-  return item.idade >= 18;
-});
-console.log(eMaior); //2.3
+console.log(mostraIdade(usuario)); // 3.3
+// Dica: Utilize uma constante pra function
 
-var empresa = usuarios.find(function (item) {
-  return item === 'Google';
-});
-console.log(empresa); //2.4
+var nome = "Diego";
+var idade = 23;
 
-var calculo = usuarios.map(function (users) {
-  return _objectSpread(_objectSpread({}, users), {}, {
-    idade: users.idade * 2
+var mostraUsuario = function mostraUsuario() {
+  var nome = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Diego';
+  var idade = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 18;
+  return {
+    nome: nome,
+    idade: idade
+  };
+};
+
+console.log(mostraUsuario(nome, idade));
+console.log(mostraUsuario(nome)); // 3.4
+
+var promise = function promise() {
+  return new Promise(function (resolve, reject) {
+    return resolve();
   });
-}).filter(function (users) {
-  return users.idade <= 50;
-});
-console.log(calculo); //usuarios.filter(item => { return item.idade*2<50; });
+};
