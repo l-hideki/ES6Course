5460ac112ffa3614b62d5a78a33c1c6de29dbe6b
unknown <lucas_hideki211@hotmail.com>
2020-06-21 13:34:36 -0300

    module 3 DONE

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    entry: './src/main.js',
+    entry: ['@babel/polyfill', './src/main.js'],
     output: {
         path: __dirname + '/public/',
         filename: 'bundle.js'
     },
-    devServer:{
-        contentBase:__dirname + '/public/'
+    devServer: {
+        contentBase: __dirname + '/public/'
     },
     module: {
         rules: [{
